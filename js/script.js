be5dfd25f8ab1bf206dba546b664764a345156a8
Gisele Silva <gisele.gomes@mirumagency.com>
2020-01-22 16:38:37 -0300

    versao desktop

--- js/script.js
+++ js/script.js
@@ -1,57 +1,56 @@
 function validacao() {
 	if(document.formuser.nome.value == "") {
-	 	alert("Please, type in your First Name.");
+	 	alert("Por favor, insira um nome.");
 		document.formuser.nome.focus();
 		return false;
 	}
 
 	if(document.formuser.phone.value == "") {
-	 	alert("Please, type in your Phone.");
+	 	alert("Por favor, insira um telefone.");
 		document.formuser.phone.focus();
 		return false;
 	}
 
 	if(document.formuser.email.value == "") {
-	 	alert("Please, type in your E-mail.");
+	 	alert("Por favor, insira um e-mail.");
 		document.formuser.email.focus();
 		return false;
 	}
 
 	if(document.formuser.email.value.indexOf('@') ==-1 || document.formuser.email.value.indexOf('.') == -1) {
-	alert("E-mail invalid. Please, try again.");
+	alert("E-mail inválido. Por favor, tente novamente");
 	document.formuser.email.focus();
 	return false;
 }
 
 	if(document.formuser.password.value == "") {
-	 	alert("Please, type in a Password.");
+	 	alert("Por favor, digite uma senha.");
 		document.formuser.password.focus();
 		return false;
 	}
 
 	if(document.formuser.password.value.length < 8) {
-	 	alert("The password must contain at least 8 characters. Please, try again.");
+	 	alert("A senha precisa ter no mínimo, 8 carateres. Por favor, tente novamente");
 		document.formuser.password.focus();
 		return false;
 	}
 
 	if(document.formuser.password2.value == "") {
-		alert("Please, repeat your Password.");
+		alert("Por favor, repita sua senha.");
 		document.formuser.password2.focus();
 		return false;
 	}
 
 	if(document.formuser.password2.value != document.formuser.password.value) {
-		alert("The passwords do not match. Please, try again.");
+		alert("As senhas não conferem. Por favor, tente novamente");
 		document.formuser.password.focus();
 		return false;
 	}
  }
 
 	function myfun(){
-
 		var x1=document.getElementById('nome').value;
 		var x2=document.getElementById('telefone').value;
 		var x3=document.getElementById('email').value;
-		alert("Name: "+x1+"\nPhone: "+x2+"\nE-mail Address: "+x3);
+			alert("Name: "+x1+"\nPhone: "+x2+"\nE-mail Address: "+x3);
 }
